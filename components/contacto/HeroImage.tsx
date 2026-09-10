b6810d1acf88ecaa5ee8794f327425373129c5c5
pachecoleoo <leonardoandrespacheco1998@gmail.com>
2026-09-10 17:31:18 -0300

    a ver ahora

--- components/contacto/HeroImage.tsx
+++ components/contacto/HeroImage.tsx
@@ -26,8 +26,8 @@ export default function HeroImage() {
       {/* Sombra superior para mejorar la lectura del navbar */}
       {/* Iluminación central detrás del título */}
       {/* Sombra inferior suave */}+{/* Contenido */}
-      <div className="relative z-10 flex h-full items-center justify-center px-10 md:px-6">
-        <div className="max-w-3xl text-center">
+      <div className="relative z-10 flex h-full items-center justify-center px-0 md:px-6">
+        <div className="max-w-2xl text-center">
           <h1
             className="
             hero-title
